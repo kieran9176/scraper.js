2d8e280bf4429dbe3583b0730b2bcd92e89195e3
Kieran Derfus <kieranderfus@gmail.com>
2017-08-12 14:47:22 -0400

    UPDATE: Fully functional

---
+++ scraper.jsx
@@ -0,0 +1,63 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+function links(keyword) {
+
+    let HTML = '';
+    let keywordStr = keyword;
+    if (keyword.includes(' ')) {
+        keywordStr = keyword.split(' ').join('_')
+    }
+
+    axios.get(`https://en.wikipedia.org/wiki/${keywordStr.toLowerCase()}`)
+        .then(wbpage => {
+
+            HTML = wbpage.data;
+            const $ = cheerio.load(HTML);
+
+            const result = first_p_links($, HTML).concat(all_links($, HTML));
+            console.log(result);
+        })
+}
+
+function first_p_links($, HTML) {
+    const links = new Set();
+    $('p').first().find('a').each((i, n) => {
+        links.add(n.attribs.href);
+    });
+
+    return create_arr_objs(links, HTML);
+}
+
+function all_links($, HTML) {
+    const links = new Set();
+    $('p').find('a').each((i, n) => {
+        links.add(n.attribs.href);
+    });
+
+    return create_arr_objs(links, HTML);
+}
+
+function create_arr_objs(link_set, HTML) {
+
+    return Array.from(link_set).map(link => {
+        const name = link.slice(6).replace(/_/g, ' ');
+        return {name, link}
+    })
+
+        .map(function (link_object) {
+            const matches = HTML.match(new RegExp(link_object.name, 'g'));
+            return {
+                name: link_object.name,
+                numOccur: matches ? matches.length : 0,
+                link: link_object.link
+            }
+        })
+
+        .sort((a, b) => b.numOccur - a.numOccur)
+        .filter(sorted_arr_obj => sorted_arr_obj.link.includes('#cite') === false);
+}
+
+links('neural network');
+
+
